763b54d2d26da4d51950e71cd633258f902c9a36
ej8899 <ej8899@gmail.com>
2024-07-09 12:23:02 -0400

    update: tweak backup SH script to function from local servers

--- lhl-w04d1-project/db_backup.sh
+++ lhl-w04d1-project/db_backup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-SOURCE_SERVER="webserver01"
-DEST_SERVER="backupserver01"
-MYSQL_USER="your_mysql_username"
-MYSQL_PASSWORD="your_mysql_password"
-DATABASE_NAME="your_database_name"
-BACKUP_FILE="/tmp/${DATABASE_NAME}_backup.sql"
+SOURCE_SERVER="linux-server"
+DEST_SERVER="10.0.2.8"
+MYSQL_USER="student"
+MYSQL_PASSWORD="lighthouse"
+DATABASE_NAME="prtg_test_db"
+BACKUP_FILE="./${DATABASE_NAME}_backup.sql"
 
-echo "Backing up database '${DATABASE_NAME}' on ${SOURCE_SERVER}..."
+echo "Backing up database '${DATABASE_NAME}' from ${SOURCE_SERVER}..."
+
 # export data to dump file
-ssh ${SOURCE_SERVER} "mysqldump -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${DATABASE_NAME} > ${BACKUP_FILE}"
+mysqldump -u ${MYSQL_USER} -p ${DATABASE_NAME} > ${BACKUP_FILE}
 
 # error check
 if [ $? -ne 0 ]; then
@@ -19,7 +20,7 @@ fi
 
 echo "Copying backup file to ${DEST_SERVER}..."
 # scp to copy across servers
-scp ${SOURCE_SERVER}:${BACKUP_FILE} ${DEST_SERVER}:${BACKUP_FILE}
+scp ${BACKUP_FILE} student@${DEST_SERVER}:${BACKUP_FILE}
 
 # error check
 if [ $? -ne 0 ]; then
@@ -29,8 +30,8 @@ fi
 
 
 echo "Cleaning up backup file on ${SOURCE_SERVER}..."
-# remove dump file on source server
-ssh ${SOURCE_SERVER} "rm -f ${BACKUP_FILE}"
+# remove dump file on source 
+rm -f ${BACKUP_FILE}
 
 # fin
 echo "Database backup '${DATABASE_NAME}' successfully copied from ${SOURCE_SERVER} to ${DEST_SERVER}"
