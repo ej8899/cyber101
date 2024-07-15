c2c38ee3d3c2fd1a87f9d8d125157bad29a8a976
ej8899 <ej8899@gmail.com>
2024-07-15 12:27:02 -0400

    add: reviewing log files in bash

---
+++ lhl-w04d1-project/bash_and_regex.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# chmod 755 sample_in_bash.sh
+# this sets the permissions of the file to 755 (read, write, execute) 
+
+# Pick a log file to search - comment the other one out.
+LOG_FILE="apache_logs.txt"
+# LOG_FILE="audit.log"
+
+clear
+echo "This is our wp-admin access attempt finder."
+
+# not needed, but this gracefully exits if the log file does not exist.
+if [ ! -f "$LOG_FILE" ]; then
+    echo "The file $LOG_FILE does not exist."
+    exit 1
+fi
+
+# just debugging - lets see what is in $LOG_FILE
+echo "the log file is $LOG_FILE"
+
+
+# uncomment any of the following code lines to see how things work
+
+# egrep " 404 " "$LOG_FILE"
+
+# egrep -e '([0-9]{3}\.)' "$LOG_FILE"
+
+# IP searching in egrep - we'll find 183.*.*.*
+# egrep -e '183\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' "$LOG_FILE"
+
+
+# the following two lines won't show on screen, 
+# BUT will create a file in the current directory called temp_wp_admin_attempts.txt
+# the output of the egrep command will be within the files.
+
+# egrep "wp-admin" "$LOG_FILE" > temp_wp_admin_attempts.txt
+# egrep "wp-admin" "$LOG_FILE"
+# egrep " 404 " "$LOG_FILE" > temp_404_errors.txt
+
+# egrep -e 'failed' "$LOG_FILE"
+
+
+# IP address "pattern matching" using "sed" and "egrep"
+
+
+
+# create a 'plain english' version of the IP pattern we want to search on.
+# you can change this in any way.
+# 150.*.*.* gives a few different IP results starting with 150
+# 208.*.*.* gives a few differetn results too
+# or you could do 208.115.*.* as another example
+# you don't need to use an IP address here either.. 
+# you can replace it with any plain text like we used in the examples above.
+IP_PATTERN="208.*.*.*"
+
+# create a regex version of the IP pattern (if an IP pattern is detected)
+# we use 'sed' which acts like a search and replace tool using regex patterns
+# so we echo the IP pattern above into "sed" and tell it to replace the dots with backslashes OR (|) any 
+# asterisks with [0-9]\{1,3\} - this is making a regex pattern out of the IP pattern we created above.
+# it is saved in the variable REGEX_PATTERN
+REGEX_PATTERN=$(echo "$IP_PATTERN" | sed 's/\./\\./g' | sed 's/\*/[0-9]\{1,3\}/g')
+# show us the Regex pattern
+echo "The regex pattern is $REGEX_PATTERN"
+# search the log file for the Regex pattern and show results on screen.
+# -o is an option that only shows the matching pattern, not the whole line.
+# egrep -o "$REGEX_PATTERN" "$LOG_FILE"
+# egrep without the -o shows us the full line
+egrep "$REGEX_PATTERN" "$LOG_FILE"
